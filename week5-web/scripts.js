502f8612782c0039c2c4058822c038070700cf06
sammic432 <sammic432@gmail.com>
2016-07-29 15:23:42 -0700

    Added files for GWC

--- week5-web/scripts.js
+++ week5-web/scripts.js
@@ -2,6 +2,26 @@ window.onload = function(){
 	alert ('hi!');
 }
 
+// <script>
+// 	var slideIndex = 1;
+// 	showDivs(slideIndex);
+
+// 	function plusDivs(n) {
+// 	    showDivs(slideIndex += n);
+// 	}
+
+// 	function showDivs(n) {
+// 	    var i;
+// 	    var x = document.getElementsByClassName("mySlides");
+// 	    if (n > x.length) {slideIndex = 1} 
+// 	    if (n < 1) {slideIndex = x.length} ;
+// 	    for (i = 0; i < x.length; i++) {
+// 	        x[i].style.display = "none"; 
+// 	    }
+// 	    x[slideIndex-1].style.display = "block"; 
+// 	}
+// </script>
+
 // $("button").click(function(){
 //     $("div").animate({
 //         left: '250px',
